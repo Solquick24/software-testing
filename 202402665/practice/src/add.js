4f91c0d2aca07df4aed3899549d6878df275945b
wlsgk7779 <wlsgk7779@o.cnu.ac.kr>
2026-05-24 13:18:13 +0900

    202402665

---
+++ 202402665/practice/src/add.js
@@ -0,0 +1,19 @@
+// function add(a,b){
+//     if(typeof a!=="number" || typeof b !== "number"){
+//         throw new Error("number only");
+//     }
+
+//     return a+b;
+// }
+function add(a,b){
+    const numA = Number(a);
+    const numB = Number(b);
+
+    if(isNaN(numA)||isNaN(numB)){
+        throw new Error("invalid input");
+    }
+    
+    return numA + numB
+}
+
+module.exports = add;
